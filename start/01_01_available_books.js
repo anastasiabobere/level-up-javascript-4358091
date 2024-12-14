70063c1d641ddec6988dd088462eb099330ee0f1
anastasia <113621721+anastasiabobere@users.noreply.github.com>
2024-12-14 17:13:48 +0000

    first challange

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,33 @@
-// Write your code here
+class Book {
+    constructor(title, author, ISBN, numCopies) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.numCopies = numCopies;
+    }
+    getAvaibility() {
+        if ((this.numCopies = 0)) {
+            return "Out of stock";
+        } else if (this.numCopies < 10) {
+            return "Low stock";
+        } else {
+            return "In stock";
+        }
+    }
+    sell(numCopies) {
+        if ((numCopies = null)) {
+            this.numCopies - 1;
+        } else {
+            this.numCopies - numCopies;
+        }
+    }
+    restock(numCopies) {
+        if ((numCopies = null)) {
+            this.numCopies + 5;
+        } else {
+            this.numCopies + numCopies;
+        }
+    }
+}
+const harryPotter = new Book("Harry Potter", "ROwling", 829818, 20)
+console.log(harryPotter)
