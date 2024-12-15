4f00c91a1a48a6940201804e6954696e252b4a2c
anastasia <113621721+anastasiabobere@users.noreply.github.com>
2024-12-15 18:17:51 +0000

    second challenge

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,17 @@
 // Write your code here
+class Movie{
+  constructor(title, director,genre,releaseYear,rating){
+    this.title= title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+  getOverview(){
+   const overview =`${this.title}, a ${this.genre} directed by ${this.director} was 
+   released in ${this.releaseYear}. It received a raiting of ${this.rating}`
+    console.log(overview)
+  }
+}
+const harryPotter = new Movie("Harry Potter", "Christopher Colombus", "Fantasy","2001",5)
+harryPotter.getOverview()
