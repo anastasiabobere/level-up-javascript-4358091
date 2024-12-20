ee1ad675d605d767eaf38e6919fb70a8b2f9c853
anastasia <113621721+anastasiabobere@users.noreply.github.com>
2024-12-20 21:41:54 +0000

    fifth challenge

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,10 @@
 // Write your code here
+const password = Symbol("password")
+const username = Symbol("username")
+const user = {
+  [username] : "Leo",
+  [password] : "3921id",
+  age:20
+}
+console.log(user.age)
+console.log(user.username) //undefined
