11b7f28178fa0da8e6dea9b740a155ea8d915272
anastasia <113621721+anastasiabobere@users.noreply.github.com>
2024-12-23 22:22:56 +0000

    seventh challenge

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,14 @@
-// Write your code here
+function getStudents(classRoom){
+  let teacher, assistiant, students
+if(classRoom.hasTeach){
+  [teacher, assistiant, ...students]= classRoom.arrays
+}else{
+  [teacher, ...students]=classRoom.arrays
+}
+console.log(students)
+}
+const classs = {
+  hasTeach: true,
+  arrays :["Ronda","Amelia", "Anton","Goroh","Nadja"]
+}
+getStudents(classs)
