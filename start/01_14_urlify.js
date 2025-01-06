6c550e5d3419af45b6e1840fd4e48cb73a81db2f
anastasia <113621721+anastasiabobere@users.noreply.github.com>
2025-01-06 18:02:10 +0000

    14th challenge

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,8 @@
 // Write your code here
+function urlify(blogTitle){
+  const punctuation = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g;
+  const blogTitleWithoutPunctuation = blogTitle.replace(punctuation, '')
+ return blogTitleWithoutPunctuation.toLowerCase().trim().replaceAll(" ", "-")
+}
+console.log(urlify("Emma's new Blog!"))
+console.log(urlify("Anastasias {code} Blog!!!"))
