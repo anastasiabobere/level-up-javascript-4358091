9f4116d234c06184009294865756e86add0e0898
anastasia <113621721+anastasiabobere@users.noreply.github.com>
2025-01-06 18:13:21 +0000

    last challenge

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,13 @@
 // Write your code here
+function checkPassword(password){
+  let oneLowerCaseLetter= /(?=.+[a-z])/
+  let oneUpperCaseLetter= /(?=.+[A-Z])/
+  let oneNumber= /(?=.+[0-9])/
+  let oneSpecialCharacter= /(?=.+[!@$#%^&*])/
+  let minimumEightCharacters = /(?=.{8,})/;
+  let isValid =
+  oneLowerCaseLetter.test(password) && oneUpperCaseLetter.test(password) && oneNumber.test(password) && oneSpecialCharacter.test(password) && minimumEightCharacters.test(password)
+  return isValid ? "Password is valid" : "not valid"
+}
+console.log(checkPassword("abc"));
+console.log(checkPassword("9Ab!4567"))
