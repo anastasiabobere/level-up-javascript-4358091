23fad1e811e104656508230b322850de39d9e47f
anastasia <113621721+anastasiabobere@users.noreply.github.com>
2025-01-06 17:46:10 +0000

    12 challenge

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,10 @@
 // Write your code here
+function numPermutations(letters){
+  if(letters.length === 1){
+    return 1
+  }
+  else{
+  return letters.length * numPermutations(letters.slice(1));
+  }
+}
+console.log(numPermutations("hello"))
